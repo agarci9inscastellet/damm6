7237c283ce6d382425abd57fdbe962578085f881
alexinfopruna <alexinfopruna@gmail.com>
2025-11-24 16:09:47 +0100

    hibernate user

--- M6UF2/MVC/xmlreader/DAO.java
+++
@@ -1,6 +0,0 @@
-import java.util.List;
-
-public interface DAO<T> {
-   void add(T item);
-   List<T> getAll();
-}
--- M6UF2/MVC/xmlreader/User.java
+++
@@ -1,9 +0,0 @@
-public class User  {
-	String name="";
-	String password="";
-	
-	public User(String pname, String ppassword) {
-		name=pname;
-		password=ppassword;
-	}
-}
