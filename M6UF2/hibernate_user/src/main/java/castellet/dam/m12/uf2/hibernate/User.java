f7bc1dba2e127ce8dd52c39c7603955d61e5779b
alexinfopruna <alexinfopruna@gmail.com>
2025-11-24 17:41:31 +0100

    many to one

--- M6UF2/hibernate_user/src/main/java/castellet/dam/m12/uf2/hibernate/User.java
+++ M6UF2/hibernate_user/src/main/java/castellet/dam/m12/uf2/hibernate/User.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,6 +20,19 @@ public class User {
 
     @Column(name = "name")
     private String name;
+
+
+    /*************************************************************
+     * Imaginem que tenim una entitat Sport
+     * 
+     * Imaginem que un usuari pot tenir un esport preferit
+     * Vinculem les dues entitats
+     * amb una relació ManyToOne (molts usuaris poden tenir el mateix esport preferit)
+     ************************************************************/
+    // @ManyToOne
+    // @JoinColumn(name = "sport_id")
+    // private Sport sport;
+
 
     // Constructor
     public User() {}
