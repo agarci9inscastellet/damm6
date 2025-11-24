2c912f732a8fef73f92134ba8d0c9ce3d10d1e0e
alexinfopruna <alexinfopruna@gmail.com>
2025-11-24 17:34:00 +0100

    fora lios

--- M6UF1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    int[] n = {1,2,3,4,5,6,7,8,9,10};
-    
-    for (int i : n) {
-      System.out.println("Number: " + i);
-    }
-  }
-}
